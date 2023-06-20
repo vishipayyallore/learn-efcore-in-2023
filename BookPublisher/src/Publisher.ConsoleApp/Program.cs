6ff23427a337512b23abf75b56cce5dbdb495c21
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-06-20 17:21:31 +0530

    Update Program.cs

--- BookPublisher/src/Publisher.ConsoleApp/Program.cs
+++ BookPublisher/src/Publisher.ConsoleApp/Program.cs
@@ -1,9 +1,15 @@
-﻿using Publisher.Data;
+﻿using Microsoft.EntityFrameworkCore;
+using Publisher.Data;
+using Publisher.Domain;
 
 using (PublisherDbContext context = new())
 {
     context.Database.EnsureCreated();
 }
+
+GetAuthors();
+
+AddAuthor(new Author { FirstName = "Sri", LastName = "Varu" });
 
 GetAuthors();
 
@@ -13,8 +19,17 @@ static void GetAuthors()
 {
     using var context = new PublisherDbContext();
     var authors = context.Authors.ToList();
+
     foreach (var author in authors)
     {
-        Console.WriteLine(author.FirstName + " " + author.LastName);
+        Console.WriteLine($"{author.Id} {author.FirstName} {author.LastName}");
     }
+}
+
+static void AddAuthor(Author author)
+{
+    using var context = new PublisherDbContext();
+
+    context.Authors.Add(author);
+    context.SaveChanges();
 }
