c16466a88dca49c43f5b6b0cb0b2a0499e5d21fb
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-06-20 17:29:52 +0530

    Update Program.cs

--- BookPublisher/src/Publisher.ConsoleApp/Program.cs
+++ BookPublisher/src/Publisher.ConsoleApp/Program.cs
@@ -13,6 +13,14 @@ AddAuthor(new Author { FirstName = "Sri", LastName = "Varu" });
 
 GetAuthors();
 
+AddAuthorWithBook(new Author { FirstName = "Julie", LastName = "Lerman" },
+    new Book[]
+    {
+        new Book { Title = "Programming Entity Framework", PublishDate = new DateTime(2009, 1, 1) },
+        new Book { Title = "Programming Entity Framework 2nd Ed", PublishDate = new DateTime(2010, 8, 1) }
+    });
+
+GetAuthors();
 // ************************ ************************\\
 
 static void GetAuthors()
@@ -22,12 +30,28 @@ static void GetAuthors()
 
     foreach (var author in authors)
     {
-        Console.WriteLine($"{author.Id} {author.FirstName} {author.LastName}");
+        Console.WriteLine($"{author.Id} {author.FirstName} {author.LastName} --");
+        foreach (var book in author.Books)
+        {
+            Console.WriteLine($" {book.BookId}. {book.Title}");
+        }
     }
 }
 
 static void AddAuthor(Author author)
 {
+    using var context = new PublisherDbContext();
+
+    context.Authors.Add(author);
+    context.SaveChanges();
+}
+
+static void AddAuthorWithBook(Author author, Book[] books)
+{
+    foreach (var book in books)
+    {
+        author.Books.Add(book);
+    }
     using var context = new PublisherDbContext();
 
     context.Authors.Add(author);
