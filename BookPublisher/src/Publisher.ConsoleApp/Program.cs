919fa9a0ce4441b172f0188330b62bc3bd9ba557
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-06-20 15:50:23 +0530

    Code Refactoring

--- BookPublisher/src/Publisher.ConsoleApp/Program.cs
+++ BookPublisher/src/Publisher.ConsoleApp/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Publisher.Data;
+
+using (PublisherDbContext context = new())
+{
+    context.Database.EnsureCreated();
+}
+
+GetAuthors();
+
+// ************************ ************************\\
+
+static void GetAuthors()
+{
+    using var context = new PublisherDbContext();
+    var authors = context.Authors.ToList();
+    foreach (var author in authors)
+    {
+        Console.WriteLine(author.FirstName + " " + author.LastName);
+    }
+}
