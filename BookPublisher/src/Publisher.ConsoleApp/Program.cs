1cfdb04113b9107a3a5ac7fdb3ddee209e1fb3e9
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-06-23 12:54:41 +0530

    Code Refactoring (#2)

--- BookPublisher/src/Publisher.ConsoleApp/Program.cs
+++ BookPublisher/src/Publisher.ConsoleApp/Program.cs
@@ -1,4 +1,4 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Publisher.ConsoleApp.Repositories;
 using Publisher.Data;
 using Publisher.Domain;
 
@@ -7,75 +7,25 @@ using (PublisherDbContext context = new())
     context.Database.EnsureCreated();
 }
 
-GetAuthors();
+AuthorsRepository.GetAuthors();
 
-AddAuthor(new Author { FirstName = "Sri", LastName = "Varu" });
-AddAuthor(new Author { FirstName = "Scott", LastName = "Rudy" });
+AuthorsRepository.AddAuthor(new Author { FirstName = "Sri", LastName = "Varu" });
+AuthorsRepository.AddAuthor(new Author { FirstName = "Scott", LastName = "Rudy" });
 
-GetAuthors();
+AuthorsRepository.GetAuthors();
 
-AddAuthorWithBook(new Author { FirstName = "Julie", LastName = "Lerman" },
+AuthorsRepository.AddAuthorWithBook(new Author { FirstName = "Julie", LastName = "Lerman" },
     new Book[]
     {
         new Book { Title = "Programming Entity Framework", PublishDate = new DateTime(2009, 1, 1) },
         new Book { Title = "Programming Entity Framework 2nd Ed", PublishDate = new DateTime(2010, 8, 1) }
     });
 
-GetAuthorsWithBooks();
+AuthorsRepository.GetAuthorsWithBooks();
 
 ResetColor();
 
 WriteLine("\n\nPress any key ...");
 ReadKey();
 
-// ************************ ************************\\
-
-static void GetAuthors()
-{
-    using var context = new PublisherDbContext();
-    var authors = context.Authors.ToList();
-
-    foreach (var author in authors)
-    {
-        ForegroundColor = ConsoleColor.Blue;
-        WriteLine($"{author.Id} {author.FirstName} {author.LastName} --");
-    }
-}
-
-static void AddAuthor(Author author)
-{
-    using var context = new PublisherDbContext();
-
-    context.Authors.Add(author);
-    context.SaveChanges();
-}
-
-static void AddAuthorWithBook(Author author, Book[] books)
-{
-    foreach (var book in books)
-    {
-        author.Books.Add(book);
-    }
-    using var context = new PublisherDbContext();
-
-    context.Authors.Add(author);
-    context.SaveChanges();
-}
-
-static void GetAuthorsWithBooks()
-{
-    using var context = new PublisherDbContext();
-
-    var authors = context.Authors.Include(a => a.Books).ToList();
-    foreach (var author in authors)
-    {
-        ForegroundColor = ConsoleColor.Yellow;
-        WriteLine($"{author.Id} {author.FirstName} {author.LastName} --");
-        foreach (var book in author.Books)
-        {
-            ForegroundColor = ConsoleColor.Cyan;
-            WriteLine($"\t{book.BookId}. {book.Title}");
-        }
-    }
-}
 
