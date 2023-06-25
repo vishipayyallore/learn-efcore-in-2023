3bc74d49ef690a5a2816c9f3cf9dd52c9b653d6b
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-06-25 18:39:45 +0530

    Update Program.cs

--- BookPublisher/src/Publisher.ConsoleApp/Program.cs
+++ BookPublisher/src/Publisher.ConsoleApp/Program.cs
@@ -8,16 +8,9 @@ AuthorsRepository.GetAuthors();
 
 AuthorsRepository.AddAuthor(GetAuthor("Sri", "Varu"));
 AuthorsRepository.AddAuthor(GetAuthor("Scott", "Rudy"));
-
 AuthorsRepository.GetAuthors();
 
-AuthorsRepository.AddAuthorWithBook(GetAuthor("Julie", "Lerman"),
-    new Book[]
-    {
-        new Book { Title = "Programming Entity Framework", PublishDate = new DateTime(2009, 1, 1) },
-        new Book { Title = "Programming Entity Framework 2nd Ed", PublishDate = new DateTime(2010, 8, 1) }
-    });
-
+AuthorsRepository.AddAuthorWithBook(GetAuthor("Julie", "Lerman"), GetFewBooks());
 AuthorsRepository.GetAuthorsWithBooks();
 
 ResetColor();
@@ -25,11 +18,20 @@ ResetColor();
 WriteLine("\n\nPress any key ...");
 ReadKey();
 
-static Author GetAuthor(string firstName, string lastName) =>
-    new() { FirstName = firstName, LastName = lastName };
-
 static void EnsureDbCreated()
 {
     using PublisherDbContext context = new();
     _ = context.Database.EnsureCreated();
 }
+
+static Author GetAuthor(string firstName, string lastName) =>
+    new() { FirstName = firstName, LastName = lastName };
+
+static Book GetBook(string title, DateTime publishDate) =>
+    new() { Title = title, PublishDate = publishDate };
+
+static Book[] GetFewBooks() => new Book[]
+        {
+            GetBook("Programming Entity Framework", new DateTime(2009, 1, 1)),
+            GetBook("Programming Entity Framework 2nd Ed", new DateTime(2010, 8, 1))
+        };
